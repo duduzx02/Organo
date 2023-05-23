ebea9c11db7658988e89d425857ab1ac18b83a8e
duduzx02 <duduzx02@gmail.com>
2023-05-23 00:15:58 -0300

    Primeiro componente: Banner

---
+++ src/components/Banner/banner.js
@@ -0,0 +1,11 @@
+import './banner.css'
+
+function Banner() {
+  return (
+    <header className="banner">
+    <img src="/images/banner.png" alt="Um banner do nosso organo" />
+    </header>
+  )
+}
+
+export default Banner
