49b8173a6d24993828e9979e1a3d9dee12c954f0
duduzx02 <duduzx02@gmail.com>
2023-05-23 01:54:14 -0300

    Explorando o projeto

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
 import Banner from './components/Banner/banner';
 
 function App() {
   return (
     <div className="App">
       <Banner/>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Bem-vindo ao React
-        </p>
-      </header>
     </div>
   );
 }
