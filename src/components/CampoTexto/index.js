f058350196fefb483147b70283f6525b3678a87c
duduzx02 <duduzx02@gmail.com>
2023-05-27 15:03:51 -0300

    Manipulando o input do usuário

--- src/components/CampoTexto/index.js
+++ src/components/CampoTexto/index.js
@@ -1,9 +1,17 @@
 import './CampoTexto.css'
 const CampoTexto = (props) => {
+
+  let valor = 'Eduardo Lima '
+
+  const aoDigitado = (e) =>{
+    valor = e.target.value
+    console.log(valor);
+  }
+
   return (
     <div className="campo-texto">
       <label>{props.label}</label>
-      <input required={props.obrigatorio} placeholder={props.placeholder} />
+      <input value={valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={props.placeholder} />
     </div>
   )
 }
