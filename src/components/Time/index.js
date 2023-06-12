766bae816289f138eba8826a3b44abc5b3f2650e
duduzx02 <duduzx02@gmail.com>
2023-06-12 01:13:17 -0300

    Refatoração do index.js TIME

--- src/components/Time/index.js
+++ src/components/Time/index.js
@@ -1,18 +1,17 @@
 import Colaborador from '../Colaborador'
 import './Time.css'
 
-const Time = (props) => {
-  const cssSection = { backgroundColor: props.corSecundaria }
-  const cssH3 = { borderColor: props.corPrimaria }
-  return (
-    (props.colaboradores.length > 0) ? <section className='time' style={cssSection}>
-      <h3 style={cssH3}>{props.nome}</h3>
-      <div className='colaboradores'>
-      {props.colaboradores.map( colaborador => <Colaborador key={colaborador.nome} corDeFundo={props.corPrimaria} nome={colaborador.nome} cargo={colaborador.cargo} imagem={colaborador.imagem}/> )}  
-      </div>
-    </section>
-    : ''
-  ) 
+const Time = ({ time, colaboradores }) => {
+    return (
+
+        colaboradores.length > 0 && <section className='time' style={{ backgroundImage: 'url(/imagens/fundo.png)', backgroundColor: time.corPrimaria }}>
+            <h3 style={{ borderColor: time.corSecundaria }}>{time.nome}</h3>
+            <div className='colaboradores'>
+                {colaboradores.map((colaborador, indice) => <Colaborador key={indice} colaborador={colaborador} corDeFundo={time.corSecundaria} />)}
+            </div>
+        </section>
+
+    )
 }
 
 export default Time
